d6173d89c6ce0bc2028ec40727643595e9ccf974
Jack Flintermann <jflinter11@gmail.com>
2012-05-03 00:18:07 -0700

    fixed header and font

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
-/* Author:
-
+/* Author: Jack Flintermann
 */
-
-
-
-
-
+var title_index = 0;
+var titles = ['has a giant forehead', 'iOS Developer at eHarmony'];
+$("#main-photo").on("click", function() {
+  $(".info-tagline").text(titles[title_index]);
+  title_index = (title_index + 1) % titles.length;
+});
